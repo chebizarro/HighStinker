9e50a967d643570b96360ec332de8a0e04689148
Chris Daley <chebizarro@gmail.com>
2019-04-15 21:38:33 -0700

    code clean up

--- high_stinker/calibrator.ino
+++ high_stinker/calibrator.ino
@@ -6,19 +6,12 @@
 */
 
 float calibrate() {
-  float sensor_volt;
-  float RS_air; 
-  float result;
   float sensorValue;
-
   for (int x = 0 ; x < 500 ; x++) {
     sensorValue = sensorValue + analogRead(A0); // Add analog values of sensor 500 times
   }
-  
   sensorValue = sensorValue / 500.0; // Take average of readings
-  sensor_volt = sensorValue * (5.0 / 1023.0); // Convert average to voltage
-  RS_air = ((5.0 * 10.0) / sensor_volt) - 10.0; // Calculate RS in fresh air
-  result = RS_air / 4.4; // Calculate results
-
-  return result;
+  float sensor_volt = sensorValue * (5.0 / 1023.0); // Convert average to voltage
+  float RS_air = ((5.0 * 10.0) / sensor_volt) - 10.0; // Calculate RS in fresh air
+  return RS_air / 4.4;
 }
--- high_stinker/high_stinker.ino
+++ high_stinker/high_stinker.ino
@@ -8,7 +8,7 @@ int led = 10; // LED pin
 int gas_sensor = A0; // Sensor pin
 float m = -0.318; // Slope
 float b = 1.133; // Y-Intercept
-float R0 = 11.820; // Sensor Resistance in fresh air
+float R0; // 11.820; // Sensor Resistance in fresh air
 
 void setup() {
   Serial.begin(9600);
@@ -19,13 +19,10 @@ void setup() {
 }
 
 void loop() {
-  float sensor_volt;
-  float RS_gas;
-  float ratio;
   float sensorValue = analogRead(gas_sensor);
-  sensor_volt = sensorValue * (5.0 / 1023.0); // Convert analog values to voltage
-  RS_gas = ((5.0 * 10.0) / sensor_volt) - 10.0; // Get value of RS in a gas
-  ratio = RS_gas / R0;
+  float sensor_volt = sensorValue * (5.0 / 1023.0); // Convert analog values to voltage
+  float RS_gas = ((5.0 * 10.0) / sensor_volt) - 10.0; // Get value of RS in a gas
+  float ratio = RS_gas / R0;
 
   double ppm_log = (log10(ratio) - b) / m; // Get ppm value in linear scale according to the the ratio value
   double ppm = pow(10, ppm_log); // Convert ppm value to log scale
